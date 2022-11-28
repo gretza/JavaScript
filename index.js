e2c34bcb78c2a5a377b673cc5dda92635dbaea2d
gret.za <greta.zaveckaite@gmail.com>
2022-11-28 18:02:24 +0200

    20221128

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// console.log - debugging
+
+// kintamieji
+// let ir const deklaravimas
+
+// naršyklės funkcijos
+// prompt, alert
+
+// duomenų tipai
+// string, number, boolean, null, undefined
+
+// tipų tikrinimas typeof
+
+// operatoriai
+// ===, +, -, *, /, >, <, **, !==, >=, <=, %
+
+// if () {},  else if () {}, else {}
+// function () {}
+
+// funkcija pasiimti elementą
+// const element = document.querySelector(".query")
+
+// elemento parametrai
+// element.innerText
+// element.className
+
 // 1.
 // const heading = document.querySelector("#my-element > h1");
 // const paragraph = document.querySelector("#my-element > p");
@@ -41,15 +67,55 @@
 
 
 // 4.
-const VISIBLE = "accordeon-content active";
-const HIDDEN = "accordeon-content";
-const dropdownElement = document.querySelector (`#accordeon .${HIDDEN}`)
+// const VISIBLE = "accordeon-content active";
+// const HIDDEN = "accordeon-content";
+// const dropdownElement = document.querySelector (`#accordeon .${HIDDEN}`)
 
-function toggle() {
-    console.log(dropdownElement.className);
-   if (dropdownElement.className  === VISIBLE) {
-    dropdownElement.className = HIDDEN;
-   } else {
-    dropdownElement.className = VISIBLE;
-   }
-}
+// function toggle() {
+//     console.log(dropdownElement.className);
+//    if (dropdownElement.className  === VISIBLE) {
+//     dropdownElement.className = HIDDEN;
+//    } else {
+//     dropdownElement.className = VISIBLE;
+//    }
+// }
+
+//20221123
+
+// 1.
+// const bodyElement = document.querySelector("body");
+
+// function changeMode() {
+//     if (bodyElement.className === "light") {
+//         bodyElement.className = "dark";
+//     } else {
+//         bodyElement.className = "light";
+//     }
+// }
+
+
+// // 1. sąlyga - jeigu šviesus fonas, tada fonas pasikeičia į tamsų
+// // šviesus fonas yra class="light"
+// // Kad pasikeistų į tamsų body klasė turi pasikeisti į class="dark"
+
+// // 2. sąlyga - jeigu tamsus fonas, pasikeičia į šviesų
+// // tamsus fonas yra class="dark"
+// // Kad pasikeistų į šviesų body klasė turi pasikeisti į class="light"
+
+// 2.
+/**
+ * Visa programa veiks tik paspaudus mygtuką.
+ * 0. cleanup, pravalome viską iš praeitų atsakymų
+ * 1. paspaudžiam mygtuką
+ * 2. paleidžiam funkciją, kuri:
+ * 3. pasiimam reikšmes iš inputų
+ * 4. patikrinam reikšmes iš inputų (>0)
+ * 5. JEIGU inputai atitinka SĄLYGĄ
+ *  ATITINKA: atlieka skaičiavimus
+ *  NEATITINKA: išmeta klaidą
+ * 
+ * KĄ TURIME NUTRINTI?:
+ * skaičiavimus
+ * senas reikšmes inputuose
+ * klaidos tekstus
+ */
