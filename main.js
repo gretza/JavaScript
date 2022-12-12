346e8af8d5ba951d8062f3811e70724aced42fe1
gret.za <greta.zaveckaite@gmail.com>
2022-12-12 18:19:22 +0200

    20221212

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-const btn = document.querySelector("#click-btn");
-const input = document.querySelector(".input");
-const inputValue = document.querySelector("#input-value");
+// const btn = document.querySelector("#click-btn");
+// const input = document.querySelector(".input");
+// const inputValue = document.querySelector("#input-value");
 
-btn.addEventListener("click", logInput);
+// btn.addEventListener("click", logInput);
 
-function logInput() {
-    const now = new Date().toLocaleString();
-    console.log(now);
-    inputValue.textContent = now;
+// function logInput() {
+//     const now = new Date().toLocaleString();
+//     console.log(now);
+//     inputValue.textContent = now;
+// }
+
+const helpBtn = document.querySelector("#help");
+const form = document.querySelector(".form-content");
+const closeBtn = document.querySelector(".close-btn");
+const firstName = document.querySelector("#f-name");
+const lastName = document.querySelector("#l-name");
+const email = document.querySelector("#email");
+const submitBtn = document.querySelector("")
+
+helpBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+submitBtn
+
+function openModal() {
+    form.style.display = "block";
+}
+
+function closeModal() {
+    form.style.display = "none";
 }
